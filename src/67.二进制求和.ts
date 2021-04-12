2d55c0b192f9f19c5591abb0f05da8199c1990b1
wangkai <wangkai_zh@outlook.com>
2021-04-12 12:42:30 +0800

    完成题目67

---
+++ src/67.二进制求和.ts
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode.cn id=67 lang=typescript
+ *
+ * [67] 二进制求和
+ */
+
+// @lc code=start
+function addBinary(a: string, b: string): string {
+  const x = a.length;
+  const y = b.length;
+  const len = x > y ? x : y;
+  const result: string[] = [];
+  let extra = false;
+  for (let i = 0; i < len; i++) {
+    const l = x > i ? a[x - i - 1] : '0';
+    const n = y > i ? b[y - i - 1] : '0';
+    if (l === '1') {
+      if (n === '1') {
+        result.push(extra ? '1' : '0');
+        extra = true;
+      } else {
+        result.push(extra ? '0' : '1');
+      }
+    } else {
+      if (n === '1') {
+        result.push(extra ? '0' : '1');
+      } else {
+        result.push(extra ? '1' : '0');
+        extra = false;
+      }
+    }
+  }
+  if (extra) result.push('1');
+  return result.reverse().join('');
+}
+// @lc code=end
